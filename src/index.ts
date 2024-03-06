6bae16b8f219357a04a9817282b1de4f300f2c80
dieegomr <dmendes.rocha@gmail.com>
2024-03-06 15:11:29 -0300

    feat: add express setup and basic route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Funcionando...');
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working...');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
